9a0f451ca69057f7fbd691cff0b099e2aefec7da
p1xan <pixan888@gmail.com>
2022-06-15 10:47:55 +0300

    controlled Input,Select,Chekbox

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -28,5 +28,35 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     return <> <input ref={inputRef} /> <button onClick={ save }>Save</button>  - actual value: {value} </>
 }
 
+export const ControlledInput = () => {
+
+    const [parentValue, setParentValue] = useState("")
+    const onChange = (e:ChangeEvent<HTMLInputElement>) => {setParentValue(e.currentTarget.value)}
+    return <input value={parentValue} onChange={onChange}/>
+}
+
+export const ControlledChekbox = () => {
+
+    const [parentValue, setParentValue] = useState(true)
+
+    const onChange = (e:ChangeEvent<HTMLInputElement>) => {setParentValue(e.currentTarget.checked)}
+
+    return <input type="checkbox" checked={parentValue} onChange={onChange}/>
+}
+
+export const ControlledSelect= () => {
+
+    const [parentValue, setParentValue] = useState<string | undefined>("1")
+
+   const onChange = (e:ChangeEvent<HTMLSelectElement>) => {setParentValue(e.currentTarget.value)}
+
+    return <>
+        <select value={parentValue} onChange={onChange}>
+            <option value={"1"}>Minsk</option>
+            <option value={"2"}>Kiev</option>
+            <option value={"3"}>Kharkov</option>
+        </select>
+        </>
+}
 
 export const ControlledInputWithFixedValue = () => <input value="blabla"/>
